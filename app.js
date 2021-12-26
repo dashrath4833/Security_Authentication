e79fba1210fccd426655500d427c98936aefcf09
dashrath <dashrathsingh4833@gmail.com>
2021-12-26 13:05:54 +0530

    Inhancing hashing with salting-Bcrypt for making database almost impossibe to encrypt.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const { config } = require("dotenv")
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 mongoose.connect("mongodb://localhost:27017/userDB")
 
@@ -36,36 +37,45 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
-    })
-    newUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets")
-        }
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email:req.body.username,
+            password:hash
+        })
+        newUser.save(function(err){
+            if(err){
+                console.log(err)
+            }else{
+                res.render("secrets")
+            }
+        })
+    });
+    
+    
 })
 
 //Simple Login Check - L1
 app.post("/login",function(req,res){
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     User.findOne({email:username},function(err,foundUser){
         if(err){
             console.log(err)
         }else{
             if(foundUser){
-                if(foundUser.password===password){
-                    res.render("secrets")
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result == true){
+                        res.render("secrets")
+                    }
+                });   
             }
         }
     })
 })
+
+
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000!!!")
